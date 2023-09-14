eaf6103756067041f4b43d01ee54d4604bf58618
pavanshivanand <108778406+pavanshivanand@users.noreply.github.com>
2023-09-14 18:45:29 +0530

    Update IDatabase.java

--- onlinebookstore-J2EE/OnlineBookStore/constants/IDatabase.java
+++ onlinebookstore-J2EE/OnlineBookStore/constants/IDatabase.java
@@ -3,7 +3,7 @@ package constants;
 public class IDatabase {
 
 	public static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
-	public static final String CONNECTION_STRING = "jdbc:mysql://javadatabasetest-server.mysql.database.azure.com:3306/javadatabasetest-database";
+	public static final String CONNECTION_STRING = "AZURE_MYSQL_CONNECTIONSTRING";
 	public static final String USER_NAME = "fuqnonploh";
 	public static final String PASSWORD = "UH6MK0TT3CJXGA70$";
 
